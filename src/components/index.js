217b21ec3ad6bff0c5c4b2108fd63a8ee4121a47
albixhuveli <albi.xhuveli.601@gmail.com>
2023-05-11 16:16:17 -0400

    3d render functional

--- src/components/index.js
+++ src/components/index.js
@@ -7,6 +7,8 @@ import Experience from './Experience';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
+import CanvasLoader from './Loader';
+
 
 export {
   Hero,
@@ -17,8 +19,9 @@ export {
   Works,
   Feedbacks,
   Contact,
+  CanvasLoader,
   EarthCanvas, 
   BallCanvas, 
   ComputersCanvas, 
   StarsCanvas
-}
+};
