7a01da1462def32607d2ab41782d02fecb0744b7
albixhuveli <albi.xhuveli.601@gmail.com>
2024-06-06 17:29:43 -0400

    fixed text bug

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -41,7 +41,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3x1 leading-[30px]'
       >
-       I'm a skilled software developer with experience in Java, Python, C++ and JavaScript, and expertise in frameworks like React and Node.js. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life! 
+       I'm a skilled software developer with experience in Java, Python, C# and JavaScript. I'm a quick learner and collaborate closely with clients to create efficient, scalable, and user-friendly solutions that solve real-world problems. Let's work together to bring your ideas to life! 
       </motion.p>
 
       <div className='mt-20 flex flex flex-wrap gap-10'>
