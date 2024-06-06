bddadb20f579ad88e877481751781fd847138b67
albixhuveli <albi.xhuveli.601@gmail.com>
2024-06-06 17:27:05 -0400

    final fix, image should be PNG not SVG file

--- src/assets/index.js
+++ src/assets/index.js
@@ -16,7 +16,7 @@ import javascript from "./tech/javascript.png";
 import net from "./tech/net.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import sql from "./tech/sql.svg";
+import sql from "./tech/sql.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
